4040be5aec738e53d1515ef243cab689c9c75d06
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2026-02-04 19:46:17 -0800

    chore: done

--- dist/ex03.js
+++ dist/ex03.js
@@ -1,12 +1,11 @@
+"use strict";
 // Exercise: Write a function `logValues` that takes an array of any type of values (numbers, strings, booleans, etc.).
 // The function should iterate through the array and log each value along with its type.
-
-function logValues(values: any[]) {
-  values.forEach((value) => {
-    console.log(`${value} (${typeof value})`);
-  });
+function logValues(values) {
+    values.forEach((value) => {
+        console.log(`${value} (${typeof value})`);
+    });
 }
-
 logValues([42, "Hello", true, { name: "Alice" }]);
 // Expected output:
 // 42 (number)
--- dist/ex04.js
+++ dist/ex04.js
@@ -1,11 +1,8 @@
+"use strict";
 // Exercise: Create a function `formatInput` that accepts either a string or number as input.
 // If it's a number, return it multiplied by 100. If it's a string, return it in uppercase.
-
-type StrOrNum = string | number;
-
-function formatInput(input: StrOrNum): StrOrNum {
-  return typeof input === "number" ? input * 100 : input.toUpperCase();
+function formatInput(input) {
+    return typeof input === "number" ? input * 100 : input.toUpperCase();
 }
-
 console.log(formatInput(5)); // Expected output: 500
 console.log(formatInput("hello")); // Expected output: "HELLO"
